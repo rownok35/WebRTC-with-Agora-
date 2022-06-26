d470009d0ec55601b67559e9a5f1e7382785a7c5
rownok35 <rownokrezwan@gmail.com>
2022-06-26 17:51:31 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+You can visit and experience it ->
+
+# PeerChat
+A peer to peer WebRTC application with controls
+
+# Installation
+* 1 - Create an account on agora.io and create an app to generate an APP ID
+* 2 - Update APP ID, Temp Token and Channel Name in main.js
+```javascript
+let APP_ID = "YOU-APP-ID"
+```
+
+# Room Page
+<img src="lobby.png">  
+
+# Video Call Page
+<img src="video.png"> 
+
+# Methodology
+<img src="methology.png"> 
---
+++ main.js
@@ -0,0 +1,213 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API 'good resource for webRtc' 
+
+/* SDP is Session Description protocol which is an object containing information about thr session connection -
+ about the codec, address, media type, audio, video and so on */
+
+/* ICE candidate is a public IP address and port that could potentially be an address that receive data   */
+
+//Instead of using our signaling server and websocket manually we have used Agora 
+
+// Agora App_ID
+// Use your App ID
+let APP_ID = "78012964079d490f99823a4cf6ddb759"
+
+
+let token = null;
+let uid = String(Math.floor(Math.random() * 10000))
+
+// client initialize the Agora functionality
+let client;
+// channel where the two users actually join
+let channel;
+
+let queryString = window.location.search
+let urlParams = new URLSearchParams(queryString)
+let roomId = urlParams.get('room')
+
+// if the room id is none then redirect to the lobby.html
+if(!roomId){
+    window.location = 'lobby.html'
+}
+
+// localStream used for local cameras video feed and microphone's audio
+let localStream;
+// remoteStream used for remote cameras video feed and microphone's audio
+let remoteStream;
+// peerConnection used for storing all the information between us and remote peer
+let peerConnection;
+
+// stun server
+const servers = {
+    iceServers:[
+        {
+            urls:['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
+        }
+    ]
+}
+
+// improves quality and control of the video
+let constraints = {
+    video:{
+        width:{min:640, ideal:1920, max:1920},
+        height:{min:480, ideal:1080, max:1080},
+    },
+    audio:true
+}
+
+
+
+let init = async () => {
+    client = await AgoraRTM.createInstance(APP_ID)
+    await client.login({uid, token})
+
+    channel = client.createChannel(roomId)
+    await channel.join()
+
+    channel.on('MemberJoined', handleUserJoined)
+    //when a user left
+    channel.on('MemberLeft', handleUserLeft)
+
+    client.on('MessageFromPeer', handleMessageFromPeer)
+
+    //getUserMedia asks user to access permission for camera and audio
+    localStream = await navigator.mediaDevices.getUserMedia(constraints)
+    document.getElementById('user-1').srcObject = localStream
+}
+ 
+
+let handleUserLeft = (MemberId) => {
+    document.getElementById('user-2').style.display = 'none'
+    document.getElementById('user-1').classList.remove('smallFrame')
+}
+
+let handleMessageFromPeer = async (message, MemberId) => {
+
+    message = JSON.parse(message.text)
+
+    if(message.type === 'offer'){
+        createAnswer(MemberId, message.offer)
+    }
+
+    if(message.type === 'answer'){
+        addAnswer(message.answer)
+    }
+
+    if(message.type === 'candidate'){
+        if(peerConnection){
+            peerConnection.addIceCandidate(message.candidate)
+        }
+    }
+
+
+}
+
+// By default we will get memberid
+let handleUserJoined = async (MemberId) => {
+    console.log('A new user joined the channel:', MemberId)
+    createOffer(MemberId)
+}
+
+
+let createPeerConnection = async (MemberId) => {
+    // initialising the peerconnection
+    peerConnection = new RTCPeerConnection(servers)
+
+    remoteStream = new MediaStream()
+    document.getElementById('user-2').srcObject = remoteStream
+    document.getElementById('user-2').style.display = 'block'
+
+    document.getElementById('user-1').classList.add('smallFrame')
+
+
+    if(!localStream){
+        localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+        document.getElementById('user-1').srcObject = localStream
+    }
+
+    // Adding track to local stream and peerconnection
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream)
+    })
+
+    peerConnection.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+            remoteStream.addTrack(track)
+        })
+    }
+
+    peerConnection.onicecandidate = async (event) => {
+        if(event.candidate){
+            client.sendMessageToPeer({text:JSON.stringify({'type':'candidate', 'candidate':event.candidate})}, MemberId)
+        }
+    }
+}
+
+let createOffer = async (MemberId) => {
+    await createPeerConnection(MemberId)
+
+    //Each peer connection will have an offer and answer
+    let offer = await peerConnection.createOffer()
+    // setLocalDescription triggers the peerConnection.onicecandidate
+    await peerConnection.setLocalDescription(offer)
+
+    client.sendMessageToPeer({text:JSON.stringify({'type':'offer', 'offer':offer})}, MemberId)
+}
+
+
+let createAnswer = async (MemberId, offer) => {
+    await createPeerConnection(MemberId)
+
+    await peerConnection.setRemoteDescription(offer)
+
+    
+    let answer = await peerConnection.createAnswer()
+    await peerConnection.setLocalDescription(answer)
+
+    client.sendMessageToPeer({text:JSON.stringify({'type':'answer', 'answer':answer})}, MemberId)
+}
+
+
+let addAnswer = async (answer) => {
+    if(!peerConnection.currentRemoteDescription){
+        peerConnection.setRemoteDescription(answer)
+    }
+}
+
+
+let leaveChannel = async () => {
+    await channel.leave()
+    await client.logout()
+}
+
+// Camera button functionality 
+let toggleCamera = async () => {
+    let videoTrack = localStream.getTracks().find(track => track.kind === 'video')
+
+    if(videoTrack.enabled){
+        videoTrack.enabled = false
+        document.getElementById('camera-btn').style.backgroundColor = 'rgb(255, 80, 80)'
+    }else{
+        videoTrack.enabled = true
+        document.getElementById('camera-btn').style.backgroundColor = 'rgb(179, 102, 249, .9)'
+    }
+}
+
+// Mic button functionality 
+let toggleMic = async () => {
+    let audioTrack = localStream.getTracks().find(track => track.kind === 'audio')
+
+    if(audioTrack.enabled){
+        audioTrack.enabled = false
+        document.getElementById('mic-btn').style.backgroundColor = 'rgb(255, 80, 80)'
+    }else{
+        audioTrack.enabled = true
+        document.getElementById('mic-btn').style.backgroundColor = 'rgb(179, 102, 249, .9)'
+    }
+}
+  
+window.addEventListener('beforeunload', leaveChannel)
+
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+document.getElementById('mic-btn').addEventListener('click', toggleMic)
+
+init()
